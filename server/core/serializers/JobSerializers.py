85a283cb9030b5f36b29edd3d98da243ac32f398
druckhead <daniel32raz@gmail.com>
2023-03-21 17:29:17 +0200

    define JobSerializers

--- server/core/serializers/JobSerializers.py
+++ server/core/serializers/JobSerializers.py
@@ -1,5 +1,27 @@
 from rest_framework import serializers
 
+from .CompanySerializers import CompanySerializer
+
+from ..models import Job
+
 
 class JobSerializer(serializers.ModelSerializer):
-    pass
+    company = CompanySerializer()
+
+    class Meta:
+        model = Job
+        fields = "__all__"
+
+
+class CreateJobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ["title", "seniority"]
+
+    def create(self, validated_data):
+        job = Job(
+            title=validated_data["title"],
+            seniority=validated_data["seniority"],
+        )
+        job.save()
+        return job
