974657214362e693408aa0f3ff0d06f1f62bb5ed
druckhead <daniel32raz@gmail.com>
2023-03-21 13:01:08 +0200

    define CreateUserSerializer

--- server/core/serializers/UserSerializers.py
+++ server/core/serializers/UserSerializers.py
@@ -9,5 +9,28 @@ class UserSerializer(serializers.ModelSerializer):
         exclude = ["password"]
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    pass
+class CreateUserSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(max_length=128, write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password",
+            "confirm_password",
+            "first_name",
+            "last_name",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
