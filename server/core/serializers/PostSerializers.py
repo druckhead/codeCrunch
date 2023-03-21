a16621242d4f088596edbafef771aa0385db585c
druckhead <daniel32raz@gmail.com>
2023-03-21 17:30:11 +0200

    add JobSerializer field to PostSerializer

--- server/core/serializers/PostSerializers.py
+++ server/core/serializers/PostSerializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
+from .JobSerializers import JobSerializer
+
 from .UserSerializers import UserSerializer
 from ..models import Post, PostSolution
 
 
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    job = JobSerializer()
 
     class Meta:
         model = Post
