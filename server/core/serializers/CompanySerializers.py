5aa42f0c5b3e953f28a75a1ed4995ecfecfbf66c
druckhead <daniel32raz@gmail.com>
2023-03-21 17:28:41 +0200

    define CompanySerializers

--- server/core/serializers/CompanySerializers.py
+++ server/core/serializers/CompanySerializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
 
+from ..models import Company
+
 
 class CompanySerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Company
+        fields = "__all__"
+
+
+class CreateCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ["name", "country"]
+
+    def create(self, validated_data):
+        company = Company(
+            name=validated_data["name"],
+            country=validated_data["country"],
+        )
+        company.save()
+        return company
