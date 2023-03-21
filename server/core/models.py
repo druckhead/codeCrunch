948276491b448c78eb5809a60d70670dfc44149a
druckhead <daniel32raz@gmail.com>
2023-03-21 10:51:00 +0200

    combine Assignment & Question models into Post model

--- server/core/models.py
+++ server/core/models.py
@@ -55,43 +55,23 @@ class Job(models.Model):
         verbose_name_plural = _("jobs")
 
 
-class Question(TimeStampedModel, TitleDescriptionModel, models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+class Post(TimeStampedModel, TitleDescriptionModel, models.Model):
+    user = models.ManyToManyField(User)
     job = models.ForeignKey(Job, on_delete=models.DO_NOTHING)
+    post_type = models.CharField(max_length=32)
 
     class Meta:
-        verbose_name = _("question")
-        verbose_name_plural = _("questions")
+        verbose_name = _("post")
+        verbose_name_plural = _("posts")
 
 
-class QuestionSolution(TimeStampedModel, models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+class PostSolution(TimeStampedModel, models.Model):
+    user = models.ManyToManyField(User)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     thumbs_up = models.IntegerField()
     thumbs_down = models.IntegerField()
     solution = models.TextField()
 
     class Meta:
-        verbose_name = _("question_solution")
-        verbose_name_plural = _("question_solutions")
-
-
-class Assignment(TimeStampedModel, TitleDescriptionModel, models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    job = models.ForeignKey(Job, on_delete=models.DO_NOTHING)
-
-    class Meta:
-        verbose_name = _("assignment")
-        verbose_name_plural = _("assignments")
-
-
-class AssignmentSolution(TimeStampedModel, models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
-    thumbs_up = models.IntegerField()
-    thumbs_down = models.IntegerField()
-    solution = models.TextField()
-
-    class Meta:
-        verbose_name = _("assignment_solution")
-        verbose_name_plural = _("assignment_solutions")
+        verbose_name = _("post_solution")
+        verbose_name_plural = _("post_solutions")
