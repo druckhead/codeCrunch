ed387924b5124cb02e15e40cbd05e45553c88fbc
druckhead <daniel32raz@gmail.com>
2023-03-21 23:18:59 +0200

    create User viewsets

--- server/core/views/UserViews.py
+++ server/core/views/UserViews.py
@@ -1,1 +1,21 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from ..models import User
+from ..serializers.UserSerializers import UserSerializer, CreateUserSerializer
+from . import customviewsets
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [JWTAuthentication]
+
+
+class CreateUserViewSet(customviewsets.CreateUpdateDeleteViewSet):
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = [JWTAuthentication]
