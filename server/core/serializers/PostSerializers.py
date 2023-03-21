970657d001bf0ee7ff3d8c0aa883cc182eb220dc
druckhead <daniel32raz@gmail.com>
2023-03-21 13:04:13 +0200

    replace Assignment & Question Serializers with PostSerializer

---
+++ server/core/serializers/PostSerializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+
+class PostSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    pass
+
+
+class PostSoltionSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CreatePostSolutionSerializer(serializers.ModelSerializer):
+    pass
