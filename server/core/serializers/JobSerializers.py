55bbd5a1a78ea54d585e1e7410e33b3e688f185a
druckhead <daniel32raz@gmail.com>
2023-03-26 00:00:17 +0300

    add functionality to receive a company in JobSerializer

--- server/core/serializers/JobSerializers.py
+++ server/core/serializers/JobSerializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from .CompanySerializers import CompanySerializer
 
-from ..models import Job
+from ..models import Company, Job
 
 
 class JobSerializer(serializers.ModelSerializer):
@@ -14,9 +15,11 @@ class JobSerializer(serializers.ModelSerializer):
 
 
 class CreateJobSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(max_length=128, write_only=True)
+
     class Meta:
         model = Job
-        fields = ["title", "seniority"]
+        fields = ["title", "seniority", "company_name"]
 
     def create(self, validated_data):
         job = Job(
@@ -24,4 +27,11 @@ class CreateJobSerializer(serializers.ModelSerializer):
             seniority=validated_data["seniority"],
         )
         job.save()
+        company = get_object_or_404(
+            Company.objects.all(), name=validated_data["company_name"]
+        )
+        job.company.add(company)
         return job
+
+    def save(self, **kwargs):
+        return super().save(**kwargs)
