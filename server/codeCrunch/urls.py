1f01e1bd02d217a27bddbcda673c137a5b039a20
druckhead <daniel32raz@gmail.com>
2023-03-25 15:04:14 +0300

    add token urls to codeCrunch url_conf

--- server/codeCrunch/urls.py
+++ server/codeCrunch/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token/", include("core.urls.tokens")),
     path("api/v1/", include("core.urls.urls")),
 ]
