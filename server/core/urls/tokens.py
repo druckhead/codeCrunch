f2ac24d414e13e596337686506e110ce1014de91
druckhead <daniel32raz@gmail.com>
2023-03-25 15:03:30 +0300

    configure url_conf for token views

---
+++ server/core/urls/tokens.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("obtain/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+]
