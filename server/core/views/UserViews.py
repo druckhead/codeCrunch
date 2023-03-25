b560d236bd9102889ad8f8026e1a2481bd44d3b0
druckhead <daniel32raz@gmail.com>
2023-03-25 12:42:04 +0300

    merge UserViewSet & CreateUserViewSet & override get_serializer_class

--- server/core/views/UserViews.py
+++ server/core/views/UserViews.py
@@ -1,21 +1,19 @@
+from django.core.serializers import get_serializer
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from ..models import User
-from ..serializers.UserSerializers import UserSerializer, CreateUserSerializer
-from . import customviewsets
+from ..serializers import UserSerializers as serializers
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     permission_classes = [AllowAny]
     authentication_classes = [JWTAuthentication]
 
-
-class CreateUserViewSet(customviewsets.CreateUpdateDeleteViewSet):
-    queryset = User.objects.all()
-    serializer_class = CreateUserSerializer
-    permission_classes = [AllowAny]
-    authentication_classes = [JWTAuthentication]
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return serializers.UserSerializer
+        elif self.action in ("update", "partial_update", "create"):
+            return serializers.CreateUserSerializer
