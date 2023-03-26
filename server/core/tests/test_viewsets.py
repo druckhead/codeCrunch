6822a672cc0c9c5b7f22cfa799cd9a074f6e8a02
druckhead <daniel32raz@gmail.com>
2023-03-26 09:40:15 +0300

    add test for JobViewSet (GET) list

--- server/core/tests/test_viewsets.py
+++ server/core/tests/test_viewsets.py
@@ -14,9 +14,16 @@ class JobViewSetTest(TestCase):
             username="admin", email="admin@admin.com", password="damndaniel"
         )
         self.company = Company.objects.create(name="Microsoft", country="Israel")
+        self.job = Job.objects.create(title="DevOps Engineer", seniority="Senior")
+        self.job.company.add(self.company)
 
     def test__get_job_viewset(self):
-        pass
+        request = self.factory.get("/api/v1/jobs", {"title": "DevOps Engineer"})
+        force_authenticate(request, user=self.user)
+        response = JobViewSet.as_view({"get": "list"})(request)
+        self.assertEqual(response.data[0].get("title"), "DevOps Engineer")
+        self.assertEqual(response.data[0].get("seniority"), "Senior")
+        self.assertEqual(response.status_code, HTTPStatus.OK._value_)
 
     def test_post_job_viewset(self):
         client = APIClient()
