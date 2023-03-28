5ffc64b381dd772a5d05232769a4264876f2c4e9
druckhead <daniel32raz@gmail.com>
2023-03-28 23:43:05 +0300

    override create method on viewset to grab user_id for m2m relation

--- server/core/views/PostViews.py
+++ server/core/views/PostViews.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from ..models import Post, PostSolution
@@ -14,6 +15,10 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [PostPermissions, BaseCUDPermission]
     authentication_classes = [JWTAuthentication]
 
+    def create(self, request, *args, **kwargs):
+        # TODO add user id to post on create
+        return super().create(request, *args, **kwargs)
+
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return serializers.PostSerializer
@@ -26,6 +31,17 @@ class PostSolutionViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PostSolutionSerializer
     permission_classes = [PostSolutionPermissions, BaseCUDPermission]
     authentication_classes = [JWTAuthentication]
+
+    def create(self, request, *args, **kwargs):
+        request_data = request.data.copy()
+        request_data["user_id"] = request.user.id
+        serializer = self.get_serializer(data=request_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
