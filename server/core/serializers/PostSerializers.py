adceb14859e93b5a44922ee56d1b2c2956d31d97
druckhead <daniel32raz@gmail.com>
2023-03-28 23:48:33 +0300

    override update on CreatePostSerializer to update all data except user_ids

--- server/core/serializers/PostSerializers.py
+++ server/core/serializers/PostSerializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from .JobSerializers import JobSerializer
 
 from .UserSerializers import UserSerializer
-from ..models import Post, PostSolution
+from ..models import Post, PostSolution, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,18 +14,29 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CreatePostSerializer(serializers.ModelSerializer):
+    # TODO add users to fields to update users related to posts
     class Meta:
         model = Post
-        fields = ["title", "description", "post_type"]
+        fields = ["title", "description", "post_type", "job"]
 
     def create(self, validated_data):
         post = Post(
             title=validated_data["title"],
             description=validated_data["description"],
             post_type=validated_data["post_type"],
+            job=validated_data["job"],
         )
         post.save()
         return post
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.post_type = validated_data.get("post_type", instance.post_type)
+        instance.job = validated_data.get("job", instance.job)
+        instance.save()
+
+        return instance
 
 
 class PostSolutionSerializer(serializers.ModelSerializer):
@@ -34,14 +46,21 @@ class PostSolutionSerializer(serializers.ModelSerializer):
 
 
 class CreatePostSolutionSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = PostSolution
-        fields = ["solution", "post_id"]
+        fields = ["solution", "post", "user_id"]
 
     def create(self, validated_data):
         post_solution = PostSolution(
             solution=validated_data["solution"],
-            post_id=validated_data["post_id"],
+            post=validated_data["post"],
         )
         post_solution.save()
+        post_solution.users.add(validated_data["user_id"])
         return post_solution
+
+    # def update(self, instance, validated_data):
+    #     instance.solution = validated_data.get("solution", instance.solution)
+    #     instance.post_id = validated_data.get("post_id", instance.post_id)
