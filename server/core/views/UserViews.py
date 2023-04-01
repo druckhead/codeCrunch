a377735101d1d4be79fbfa575aaf00726b017076
druckhead <daniel32raz@gmail.com>
2023-04-01 15:32:16 +0300

    override retrieve and destroy methods of ModelViewSet

--- server/core/views/UserViews.py
+++ server/core/views/UserViews.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from ..models import User
@@ -13,10 +14,26 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
     authentication_classes = [JWTAuthentication]
 
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if not instance.is_staff and not instance.is_active:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.save()
+
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             if self.request.user.is_superuser:
                 return serializers.AdminUserSerializer
             return serializers.UserSerializer
-        elif self.action in ("update", "partial_update", "create"):
+        elif self.action in ("update", "partial_update", "create", "destroy"):
             return serializers.CreateUserSerializer
