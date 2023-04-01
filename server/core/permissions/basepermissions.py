34c872392ad66981a392385aaf3c44c6ed60eb18
druckhead <daniel32raz@gmail.com>
2023-04-01 14:30:39 +0300

    fix _get_obj_or_objuser function

--- server/core/permissions/basepermissions.py
+++ server/core/permissions/basepermissions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission
 
-from ..models import Company, Job, Post, PostSolution
+from ..models import Post, PostSolution
 
 
 class BaseCUDPermission(BasePermission):
@@ -10,21 +10,17 @@ class BaseCUDPermission(BasePermission):
         return True
 
     def has_object_permission(self, request, view, obj):
-        obj_request = self._get_request_obj(request, obj)
+        obj_or_objuser = self._get_obj_or_objuser(obj)
         if view.action in ("update", "partial_update", "destroy"):
             return (
                 request.user.is_authenticated
-                and obj_request.user.id == obj.id
+                and request.user.id == obj_or_objuser.id
                 or request.user.is_authenticated
                 and request.user.is_superuser
             )
         return True
 
-    def _get_request_obj(self, request, obj):
-        return request
-        # if isinstance(obj, (Company, Job)):
-        #     return request
-        # if isinstance(obj, Post):
-        #     return request
-        # if isinstance(obj, PostSolution):
-        #     return request
+    def _get_obj_or_objuser(self, obj):
+        if isinstance(obj, (Post, PostSolution)):
+            return obj.user
+        return obj
