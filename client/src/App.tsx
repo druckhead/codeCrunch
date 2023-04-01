4042ac75b95bf213b9b65325ffe64bf21723bd5e
druckhead <daniel32raz@gmail.com>
2023-04-02 01:54:29 +0300

    add ThemeProvider in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,34 @@
-import "./App.css";
+import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Root from "./pages/Root";
-import { Home } from "@mui/icons-material";
+import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 
-function App() {
+export default function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes>
         <Route path="/" element={<Root />}>
           <Route index element={<HomePage />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
-
-export default App;
