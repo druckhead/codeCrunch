d226804464a07895fa0e904a0e44758e52eaed5f
druckhead <daniel32raz@gmail.com>
2023-04-03 13:33:19 +0300

    make navbar sticky

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
+import { Box, Container } from "@mui/material";
 
 export default function Root() {
   return (
     <React.Fragment>
-      <NavBar />
-      <Outlet />
+      <Box component="header" sx={headerStyles}>
+        <NavBar />
+      </Box>
+      <Container>
+        <Outlet />
+      </Container>
     </React.Fragment>
   );
 }
+
+const headerStyles = {
+  zIndex: 999,
+  top: 0,
+  position: "sticky",
+  mb: 5,
+};
