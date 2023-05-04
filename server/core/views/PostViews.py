f64bc1d9069b74b2e8863130f0228e803e51bf94
druckhead <daniel32raz@gmail.com>
2023-05-04 22:13:05 +0300

    remove unnecessary imports

--- server/core/views/PostViews.py
+++ server/core/views/PostViews.py
@@ -1,5 +1,4 @@
 from rest_framework import status, viewsets
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
