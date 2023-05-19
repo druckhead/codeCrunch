32b760cb1039ea2514be14de590ea92f65b5f4ff
druckhead <daniel32raz@gmail.com>
2023-05-19 16:33:01 +0300

    update to use enum instead of dictionary object

--- client/src/utils/endpointConstants.tsx
+++ client/src/utils/endpointConstants.tsx
@@ -1,28 +1,15 @@
-const BASE_API_LOCALHOST_URL = "http://127.0.0.1";
-const BASE_API_IP_URL = import.meta.env.VITE_MY_IP;
-
-const BASE_API_PORT = ":8000/";
-const BASE_API_ENDPOINT = "api/";
-const BASE_API_VERSION = "v1/";
-const FULL_API_ENDPOINT =
-  BASE_API_LOCALHOST_URL + BASE_API_PORT + BASE_API_ENDPOINT;
-
-const TOKEN = "token/";
-const USERS = "users";
-const JOBS = "jobs";
-const COMPANIES = "companies";
-const POSTS = "posts";
-const POSTSOLUTIONS = "postsolutions";
-
-export const API_ENDPOINTS = {
-  AUTH: {
-    LOGIN: FULL_API_ENDPOINT + TOKEN + "login/",
-    REFRESH: FULL_API_ENDPOINT + TOKEN + "refresh/",
-    ME: FULL_API_ENDPOINT + TOKEN + "me/",
-    VERIFY: FULL_API_ENDPOINT + TOKEN + "verify/",
-    LOGOUT: FULL_API_ENDPOINT + TOKEN + "blacklist/",
-  },
-  USERS: {
-    REGISTER: FULL_API_ENDPOINT + BASE_API_VERSION + USERS,
-  },
-};
+export enum ERoutes {
+  API_BASE_URL = `http://127.0.0.1:8000/api`,
+  V1 = `/v1`,
+  TOKEN_PREFIX = `/token`,
+  USERS_PREFIX = `/users`,
+  JOBS_PREFIX = `/jobs`,
+  COMPANIES_PREFIX = `/companies`,
+  POSTS_PREFIX = `/posts`,
+  POSTSOLUTIONS_PREFIX = `/postsolutions`,
+  LOGIN = `/login/`,
+  REFRESH = `/refresh/`,
+  ME = `/me/`,
+  VERIFY = `/verify/`,
+  LOGOUT = `blacklist/`,
+}
