05ac9668ed4c106add67bc9a9ebd018146a43bca
druckhead <daniel32raz@gmail.com>
2023-05-19 16:42:31 +0300

    split login & signup from AuthForm

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,9 +1,8 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import AuthForm from "../components/AuthForm";
 import { Link } from "react-router-dom";
-
-export type authPageProps = { isSignInOuter: boolean };
+import Login from "./Login/Login";
+import Signup from "./Signup/Signup";
 
 export default function AuthPage({ isSignInOuter }: authPageProps) {
   const [isSignIn, setIsSignIn] = useState(isSignInOuter);
@@ -14,53 +13,62 @@ export default function AuthPage({ isSignInOuter }: authPageProps) {
   }, [isSignInOuter]);
 
   return (
-    <Box>
-      <Typography
-        component="h2"
-        variant="h5"
-        fontWeight={600}
-        color="inherit"
-        noWrap
-        textAlign="center"
+    <React.Fragment>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
       >
-        {`Sign ${isSignIn ? "in to your account" : "up for free"}`}
-      </Typography>
-      <Typography
-        component="div"
-        color="inherit"
-        noWrap
-        textAlign="center"
-        mb="1em"
-      >
-        <Box display="flex" justifyContent="center">
-          <Box component="div">
-            <Typography component="span" color={theme.palette.text.primary}>
-              or
-            </Typography>
-            <Link
-              to={isSignIn ? "../sign_up" : "../sign_in"}
-              onClick={(e) => {
-                setIsSignIn((prevValue) => !prevValue);
-              }}
-            >
-              <Typography
-                component="span"
-                color="#ffa94c"
-                fontWeight={600}
-                sx={{
-                  ":hover": {
-                    color: "#ff9832",
-                  },
+        <Typography
+          component="h2"
+          variant="h5"
+          fontWeight={600}
+          color="inherit"
+          noWrap
+          textAlign="center"
+        >
+          {`Sign ${isSignIn ? "in to your account" : "up for free"}`}
+        </Typography>
+        <Typography
+          component="div"
+          color="inherit"
+          noWrap
+          textAlign="center"
+          mb="1em"
+        >
+          <Box display="flex" justifyContent="center">
+            <Box component="div">
+              <Typography component="span" color={theme.palette.text.primary}>
+                or
+              </Typography>
+              <Link
+                to={isSignIn ? "../sign_up" : "../sign_in"}
+                onClick={(e) => {
+                  setIsSignIn((prevValue) => !prevValue);
                 }}
               >
-                {" Sign"} {isSignIn ? "up " : "in "}
-                {isSignIn ? "for a new" : "to your existing"} account
-              </Typography>
-            </Link>
+                <Typography
+                  component="span"
+                  color="#ffa94c"
+                  fontWeight={600}
+                  sx={{
+                    ":hover": {
+                      color: "#ff9832",
+                    },
+                  }}
+                >
+                  {" Sign"} {isSignIn ? "up " : "in "}
+                  {isSignIn ? "for a new" : "to your existing"} account
+                </Typography>
+              </Link>
+            </Box>
           </Box>
-        </Box>
-      </Typography>
-      <AuthForm isSignIn={isSignIn} />
-    </Box>
+        </Typography>
+        {isSignIn ? <Login /> : <Signup />}
+      </Box>
+    </React.Fragment>
   );
 }
+
+export type authPageProps = { isSignInOuter: boolean };
