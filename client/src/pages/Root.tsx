60600564b1685f7fe180be9e08b5a39e089c321a
druckhead <daniel32raz@gmail.com>
2023-05-19 16:57:09 +0300

    add margin-top and margin-bottom

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/Navbar/NavBar";
 import { Outlet } from "react-router-dom";
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 
 export default function Root() {
   return (
     <React.Fragment>
       <NavBar />
-      <Container>
+      <Container sx={{ my: 2 }}>
         <Outlet />
       </Container>
     </React.Fragment>
